b36ca57c35f797f9b5936380a1e3027e70b27050
Aanal Trivedi <aanal.trivedi@innodeed.com>
2023-10-20 16:34:55 +0530

    creation

---
+++ Sources/TestGreet.xcframework/ios-arm64/TestGreet.framework/Headers/TestGreet.h
@@ -0,0 +1,18 @@
+//
+//  TestGreet.h
+//  TestGreet
+//
+//  Created by SAPPHIRE on 20/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestGreet.
+FOUNDATION_EXPORT double TestGreetVersionNumber;
+
+//! Project version string for TestGreet.
+FOUNDATION_EXPORT const unsigned char TestGreetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestGreet/PublicHeader.h>
+
+
